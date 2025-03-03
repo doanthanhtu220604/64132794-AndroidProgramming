f8ea46e180ad604368c36d9a58c4cd084059d7ec
Doan Thanh Tu <tu.dt.64cntt@ntu.edu.vn>
2025-03-03 14:59:27 +0700

    ex4

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/com/example/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/com/example/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex4_addsubmuldiv_onclick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // khai bao cac dieu khien
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
+
+    void TimDieuKhien(){
+        editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        editTextKQ = (EditText)findViewById(R.id.edtKetQua);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,102 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
     }
+
+    // xu ly cong
+    void XuLyCong (View v){
+        // xu ly cong o day
+        //b1 lay du lieu 2 so
+        // b1.1 tim edittext so 1 va so 2
+        EditText editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        EditText editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        // lay du lieu tu hai dieu khien do
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+        // chuyen tu chuoi sang so
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+        // tinh toan
+        float Tong = soA + soB ;
+
+        EditText editTextKQ = (EditText)findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+
+
+    }
+    void XuLyTru (View v){
+        // xu ly cong o day
+        //b1 lay du lieu 2 so
+        // b1.1 tim edittext so 1 va so 2
+        EditText editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        EditText editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        // lay du lieu tu hai dieu khien do
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+        // chuyen tu chuoi sang so
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+        // tinh toan
+        float Tong = soA - soB ;
+
+        EditText editTextKQ = (EditText)findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+
+
+    }
+    void XuLyNhan (View v){
+        // xu ly cong o day
+        //b1 lay du lieu 2 so
+        // b1.1 tim edittext so 1 va so 2
+        EditText editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        EditText editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        // lay du lieu tu hai dieu khien do
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+        // chuyen tu chuoi sang so
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+        // tinh toan
+        float Tong = soA * soB ;
+
+        EditText editTextKQ = (EditText)findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+
+
+    }
+    void XuLyChia (View v){
+        // xu ly cong o day
+        //b1 lay du lieu 2 so
+        // b1.1 tim edittext so 1 va so 2
+        EditText editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        EditText editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        // lay du lieu tu hai dieu khien do
+        String sothu1 = editTextSo1.getText().toString();
+        String sothu2 = editTextSo2.getText().toString();
+        // chuyen tu chuoi sang so
+        float soA = Float.parseFloat(sothu1);
+        float soB = Float.parseFloat(sothu2);
+        // tinh toan
+        float Tong = soA / soB ;
+
+        EditText editTextKQ = (EditText)findViewById(R.id.edtKetQua);
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+
+
+    }
+
+
 }
