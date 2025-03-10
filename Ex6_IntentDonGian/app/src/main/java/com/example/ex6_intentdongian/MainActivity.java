e0cbb7e97f89bf572cdda08aa4632c4c5e9e5a2c
Doan Thanh Tu <tu.dt.64cntt@ntu.edu.vn>
2025-03-10 13:47:30 +0700

    ex6

--- Ex6_IntentDonGian/app/src/main/java/com/example/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/com/example/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien (){
+        Button nutMH2 = (Button) findViewById(R.id.btnMH2);
+        Button nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // tim dieu khien nut bam
+        TimDieuKhien();
+        // gan bo lang nghe xu kien
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xu ly chuyen man hinh
+                // b1: tao intent, 2 tham so: man hinh hien tai va man hinh chuyen toi
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                // b2: gui
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                // b2: gui
+                startActivity(intentMH3);
+            }
+        });
+
     }
 }
