5aada7e44d1f4536dc61b7726e156865ec9c3550
Doan Thanh Tu <tu.dt.64cntt@ntu.edu.vn>
2025-03-29 10:44:41 +0700

    LTGK

--- LuyenTapGK/app/src/main/java/com/example/luyentapgk/ActivityCau1.java
+++ LuyenTapGK/app/src/main/java/com/example/luyentapgk/ActivityCau1.java
@@ -1,6 +1,11 @@
 package com.example.luyentapgk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityCau1 extends AppCompatActivity {
+    Button nutCong, nutTroVe;
+    EditText editTextsoA;
+    EditText editTextsoB;
+    EditText editTextKetQua;
+    void TimDieuKhien(){
+        nutTroVe = (Button) findViewById(R.id.btnTroVe) ;
+        nutCong = (Button) findViewById(R.id.btnCong);
+        editTextsoA = (EditText) findViewById(R.id.edtA);
+        editTextsoB = (EditText) findViewById(R.id.edtB);
+        editTextKetQua = (EditText) findViewById(R.id.edtKQ);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,31 @@ public class ActivityCau1 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TimDieuKhien();
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextsoA = (EditText) findViewById(R.id.edtA);
+                EditText editTextsoB = (EditText) findViewById(R.id.edtB);
+                String sothu1 = editTextsoA.getText().toString();
+                String sothu2 = editTextsoB.getText().toString();
+                float soA = Float.parseFloat(sothu1);
+                float soB = Float.parseFloat(sothu2);
+                float tong = soA + soB;
+                EditText editTextKetQua = (EditText) findViewById(R.id.edtKQ);
+                String chuoiKQ = String.valueOf(tong);
+                editTextKetQua.setText(chuoiKQ);
+
+
+            }
+        });
+        nutTroVe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTroVeMain = new Intent(ActivityCau1.this,MainActivity.class);
+                startActivity(intentTroVeMain);
+            }
         });
     }
 }
--- LuyenTapGK/app/src/main/java/com/example/luyentapgk/MainActivity.java
+++ LuyenTapGK/app/src/main/java/com/example/luyentapgk/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.luyentapgk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nutMH1, nutMH2, nutMH3, nutMH4;
+    void TimDieuKhien (){
+        nutMH1 = (Button) findViewById(R.id.btnMH1);
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+        nutMH4 = (Button) findViewById(R.id.btnMH4);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TimDieuKhien();
+        nutMH1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH1 = new Intent(MainActivity.this,ActivityCau1.class);
+                startActivity(intentMH1);
+            }
+        });
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH2 = new Intent(MainActivity.this,ActivityCau2.class);
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this,ActivityCau3.class);
+                startActivity(intentMH3);
+            }
+        });
+        nutMH4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH4 = new Intent(MainActivity.this,ActivityCau4.class);
+                startActivity(intentMH4);
+            }
         });
     }
 }
